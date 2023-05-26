fa21452778ea48be2489ffa46e15414226d3888f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-26 13:48:40 -0600

    feat(api/genre/routes): :hammer: We created de genre routes module.
    In this module we will create all the routes for the genre endpoint. We also added the routes module to the server module and linked it to the genre endpoint.

--- Form-Javascript-React-Express-PostgreSQL/api/src/server.js
+++ Form-Javascript-React-Express-PostgreSQL/api/src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const genreRouter = require('./genre/routes/Genre.routes')
+
 const server = express()
 
 server.use(express.json())
@@ -9,7 +11,9 @@ server.use(cors())
 server.use(morgan('dev'))
 
 server.get('/', (req, res) => {
-  res.status(200).send('Servidor Online')
+  res.status(200).send('Online Server')
 })
+
+server.use('/genre', genreRouter)
 
 module.exports = server
