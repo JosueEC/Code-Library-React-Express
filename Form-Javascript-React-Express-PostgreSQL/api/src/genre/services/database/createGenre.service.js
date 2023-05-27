fa2e47965d9af3a65633f532645e92bc928404e8
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-27 13:09:10 -0600

    feat(api/genre/services/database): :hammer: We created the createGenre service for our database services.
    With this service we can create new genres in the genre model.

---
+++ Form-Javascript-React-Express-PostgreSQL/api/src/genre/services/database/createGenre.service.js
@@ -0,0 +1,13 @@
+const { genre } = require('../../../../config/database')
+
+const createGenre = async ({ name }) => {
+  const createdGenre = await genre.create({
+    name
+  })
+
+  return createdGenre
+}
+
+module.exports = {
+  createGenre
+}
