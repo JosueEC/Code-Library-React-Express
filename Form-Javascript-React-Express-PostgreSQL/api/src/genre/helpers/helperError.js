2cc07ce4916eb5460e234d2f78cc26a4aefb1c23
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-26 18:17:14 -0600

    feat(api/genre/helpers): :hammer: We added a helper function to handle the errors and we also added the http status code 500 to our .env

--- Form-Javascript-React-Express-PostgreSQL/api/src/genre/helpers/helperError.js
+++ Form-Javascript-React-Express-PostgreSQL/api/src/genre/helpers/helperError.js
@@ -1,1 +1,13 @@
+require('dotenv').config()
 
+const { HTTP_INTERNAL_SERVER_ERROR } = process.env
+
+const httpError = (res, error) => {
+  console.info(error)
+  res.status(HTTP_INTERNAL_SERVER_ERROR).send({
+    code: Number(HTTP_INTERNAL_SERVER_ERROR),
+    message: error.message
+  })
+}
+
+module.exports = { httpError }
