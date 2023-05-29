b903698a3c298945ea93c7c75421553f2e5824f8
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-29 13:40:09 -0600

    feat(api/videogame/controllers): :hammer: We added the service createVideogame to our handler postVideogame.

--- Form-Javascript-React-Express-PostgreSQL/api/src/videogame/controllers/videogame.controller.js
+++ Form-Javascript-React-Express-PostgreSQL/api/src/videogame/controllers/videogame.controller.js
@@ -3,11 +3,17 @@ const { HTTP_BAD_REQUEST, HTTP_CREATED } = process.env
 
 const { httpError } = require('../../helpers/helperError')
 
+const { createVideogame } = require('../services/database/createVideogame.service')
+
 const saveVideogame = async (req, res) => {
   try {
+    const { name, description, image, released, platforms, genres } = req.body
+
+    const newVideogame = await createVideogame({ name, description, image, released, platforms, genres })
+
     res.status(HTTP_BAD_REQUEST).send({
       code: Number(HTTP_CREATED),
-      data: 'game created'
+      data: newVideogame
     })
   } catch (error) {
     httpError(res, error, HTTP_BAD_REQUEST)
