ba220e95f90c74169b8580c53cf6040a9ade27f6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-30 12:07:35 -0600

    feat(api/genre/routes): :hammer: We added the checkOrigin middleware to our postGenre route.

--- Form-Javascript-React-Express-PostgreSQL/api/src/genre/routes/Genre.routes.js
+++ Form-Javascript-React-Express-PostgreSQL/api/src/genre/routes/Genre.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 
+const { checkOrigin } = require('../../middlewares/origin')
+
 const genreController = require('../controllers/Genre.controller')
 
 const router = Router()
 
-router.post('/', genreController.postGenre)
+router.post('/', checkOrigin, genreController.postGenre)
 
 router.get('/', genreController.getGenres)
 
