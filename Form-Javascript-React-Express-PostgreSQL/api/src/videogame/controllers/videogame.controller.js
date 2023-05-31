1c9584a2efc3295f2b7b2a1b90342a3e9e940f3f
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-31 14:04:31 -0600

    feat(api/videogame/controllers): :hammer: Replaced the response action with our successHelper in all videogame handlers.

--- Form-Javascript-React-Express-PostgreSQL/api/src/videogame/controllers/videogame.controller.js
+++ Form-Javascript-React-Express-PostgreSQL/api/src/videogame/controllers/videogame.controller.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { HTTP_BAD_REQUEST, HTTP_NOT_FOUND, HTTP_CREATED, HTTP_FOUND } = process.env
 
+const { httpSuccess } = require('../../helpers/success.helper')
 const { httpError } = require('../../helpers/error.helper')
 
 const { createVideogame } = require('../services/database/createVideogame.service')
@@ -11,25 +12,16 @@ const saveVideogame = async (req, res) => {
     const { name, description, image, released, platforms, genres } = req.body
 
     const newVideogame = await createVideogame({ name, description, image, released, platforms, genres })
-
-    res.status(HTTP_BAD_REQUEST).send({
-      code: Number(HTTP_CREATED),
-      data: newVideogame
-    })
+    httpSuccess(res, newVideogame, HTTP_CREATED)
   } catch (error) {
     httpError(res, error, HTTP_BAD_REQUEST)
   }
 }
 
-// TODO: create the findAllVideogames service for this handler
 const findVideogames = async (req, res) => {
   try {
     const allVideogames = await findAllVideogames()
-
-    res.status(HTTP_FOUND).send({
-      code: Number(HTTP_FOUND),
-      data: allVideogames
-    })
+    httpSuccess(res, allVideogames, HTTP_FOUND)
   } catch (error) {
     httpError(res, error, HTTP_NOT_FOUND)
   }
